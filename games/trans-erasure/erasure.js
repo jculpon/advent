e571a9dd0fa5f45a8d48490e919785a8ae43b609
Jamie Culpon <jculpon@gmail.com>
2013-12-10 23:21:36 -0800

    Add basic word erasing
    Can now erase a set of target words in a sample text with a click.
    
    Future enhancements:
    * click -> rub out
    * support rotating through texts

--- games/trans-erasure/erasure.js
+++ games/trans-erasure/erasure.js
@@ -1,8 +1,33 @@
-var erasure = {};
+var erasure = {
+    targetWords: ['text', 'test'],
+    decorateTargets: function() {
+        /*
+         * Build a regexp based on target words:
+         *    ['foo', 'bar', 'baz'] -> \\b(foo|bar|baz)\\b
+         */
+        var delim = erasure.targetWords.reduce(function(a,b) { return a + '|' + b; });
+        var regexp = new RegExp('\\b(' + delim + ')\\b', 'g');
+
+        /*
+         * Replace the level display area with a version with the target
+         * words wrapped in the span defined by addSpan
+         * There has to be a prettier way to do this... 
+         */
+        $('#levelDisplay').html($('#levelDisplay').text().replace(regexp, erasure.addSpan));
+    },
+    addSpan: function(text) {
+        return '<span class="erasable">' + text + '</span>';
+    }
+};
 
 $(document).ready(function() {
     console.log('ready!');
-    erasure.gameArea = $('#gameArea');
-    console.log(erasure.gameArea);
+
+    erasure.decorateTargets();
+
+    $('.erasable').click(function() {
+        $(this).fadeOut();
+        return false;
+    });
 });
 
