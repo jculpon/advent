593a7d08b8041ff9628f744dd00f56b4a9592c43
Jamie Culpon <jculpon@gmail.com>
2013-12-10 23:38:57 -0800

    Basic progress updates

--- games/trans-erasure/erasure.js
+++ games/trans-erasure/erasure.js
@@ -14,10 +14,25 @@ var erasure = {
          * There has to be a prettier way to do this... 
          */
         $('#levelDisplay').html($('#levelDisplay').text().replace(regexp, erasure.addSpan));
+
+        erasure.updateProgress()
     },
     addSpan: function(text) {
         return '<span class="erasable">' + text + '</span>';
+    },
+    targetWordsTotal: function() {
+        return $('.erasable').size();
+    },
+    targetWordsRemaining: function() {
+        return $('.erasable:visible').size();
+    },
+    updateProgress: function() {
+        var remaining = erasure.targetWordsRemaining();
+        var total = erasure.targetWordsTotal();
+
+        $('#progress').text('Left: ' + remaining + '/' + total);
     }
+    
 };
 
 $(document).ready(function() {
@@ -26,7 +41,7 @@ $(document).ready(function() {
     erasure.decorateTargets();
 
     $('.erasable').click(function() {
-        $(this).fadeOut();
+        $(this).fadeOut(400, erasure.updateProgress);
         return false;
     });
 });
